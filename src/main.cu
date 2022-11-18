c332bfbbba59bc04267f4381fe6773c820687423
bjadczak <73365815+bjadczak@users.noreply.github.com>
2022-11-18 19:26:19 +0100

    Add working code on simple solution using parallel solution

--- src/main.cu
+++ src/main.cu
@@ -5,13 +5,15 @@
 #include <iostream>
 
 #include <stack>
+#include <queue>
 #include <chrono>
 
 #define N 3
 #define BOARD_SIZE (N * N)
 #define CELL_COUNT (BOARD_SIZE * BOARD_SIZE)
+#define NUM_OF_THREADS 32
 
-// #define DEBUG_MODE
+#define DEBUG_MODE
 
 struct appeared
 {
@@ -29,6 +31,12 @@ struct possibilitie
     int poss[BOARD_SIZE] = {0};
 
 } typedef possibilitie;
+struct possibleBoard
+{
+    int board[CELL_COUNT] = {0};
+    int status = 0;
+
+} typedef possibleBoard;
 struct board
 {
     int board[CELL_COUNT] = {0};
@@ -93,7 +101,7 @@ __host__ void printBoard(int *sudokuBoard)
         printf("\n");
     }
 }
-__host__ bool isBoardValid(int *sudokuBoard)
+__host__ __device__ bool isBoardValid(int *sudokuBoard)
 {
     for (int i = 0; i < BOARD_SIZE; i++)
     {
@@ -134,6 +142,54 @@ __host__ bool isBoardValid(int *sudokuBoard)
             }
             for (int k = 0; k < BOARD_SIZE; k++)
                 if (all[k] != 1)
+                {
+                    return false;
+                }
+        }
+    }
+    return true;
+}
+__device__ bool isBoardCorrect(const int *sudokuBoard)
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        int all[BOARD_SIZE] = {0};
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (sudokuBoard[i * BOARD_SIZE + j] > 0)
+                all[sudokuBoard[i * BOARD_SIZE + j] - 1]++;
+        }
+        for (int j = 0; j < BOARD_SIZE; j++)
+            if (all[j] > 1)
+                return false;
+    }
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        int all[BOARD_SIZE] = {0};
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (sudokuBoard[j * BOARD_SIZE + i] > 0)
+                all[sudokuBoard[j * BOARD_SIZE + i] - 1]++;
+        }
+        for (int j = 0; j < BOARD_SIZE; j++)
+            if (all[j] > 1)
+                return false;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int all[BOARD_SIZE] = {0};
+            for (int x = i * N; x < (i + 1) * N; x++)
+            {
+                for (int y = j * N; y < (j + 1) * N; y++)
+                {
+                    if (sudokuBoard[x * BOARD_SIZE + y] > 0)
+                        all[sudokuBoard[x * BOARD_SIZE + y] - 1]++;
+                }
+            }
+            for (int k = 0; k < BOARD_SIZE; k++)
+                if (all[k] > 1)
                 {
                     return false;
                 }
@@ -205,14 +261,14 @@ __device__ void calculatePossibilites(const int *currentBoard, int *emptyCells, 
         {
             for (int i = 0; i < BOARD_SIZE; i++)
                 poss[indx].poss[i] = emptyInAll[i];
-            poss[indx].cell = cell + 1;
+            poss[indx].cell = cell;
             indx++;
         }
 
         // Reset arrays
         for (int i = 0; i < BOARD_SIZE; i++)
         {
-            appeardInBlock[i] = appeardInColumn[i] = appeardInRow[i] = 0;
+            emptyInAll[i] = appeardInBlock[i] = appeardInColumn[i] = appeardInRow[i] = 0;
         }
         tmp = 0;
     }
@@ -220,7 +276,7 @@ __device__ void calculatePossibilites(const int *currentBoard, int *emptyCells, 
     *possCount = indx;
 }
 
-__global__ void runSolver(const int *currentBoard, possibilitie *poss)
+__global__ void runSolver(const int *currentBoard, possibleBoard *possBoard)
 {
     // Kerenl recives a board as an array size of CELL_COUNT
     // It generates valid boards that can be created
@@ -233,7 +289,13 @@ __global__ void runSolver(const int *currentBoard, possibilitie *poss)
     // i.e. how many possibilites can ther be
     int indx = 0;
     int emptyCells[CELL_COUNT] = {0};
-    poss += threadIdx.x;
+
+    currentBoard += (CELL_COUNT)*threadIdx.x;
+    possBoard += (BOARD_SIZE)*threadIdx.x;
+    if (!isBoardCorrect(currentBoard))
+        return;
+
+    possibilitie *poss = new possibilitie[CELL_COUNT];
     for (int i = 0; i < CELL_COUNT; i++)
     {
         if (currentBoard[i] == 0)
@@ -241,12 +303,52 @@ __global__ void runSolver(const int *currentBoard, possibilitie *poss)
             emptyCells[indx] = i;
             indx++;
         }
+        for (int j = 0; j < CELL_COUNT; j++)
+            possBoard[i].board[j] = currentBoard[j];
+
+        possBoard[i].status = 0;
+    }
+    for (int i = 0; i < CELL_COUNT; i++)
+    {
+        poss[i].cell = 0;
+        for (int j = 0; j < BOARD_SIZE; j++)
+            poss[i].poss[j] = 0;
     }
 
     calculatePossibilites(currentBoard, (int *)emptyCells, poss, &indx);
 
     // We now have all possible otions that can be safely inputted into our
     // current board.
+
+    int leastOption = 11, iWithLeastOptions = -1;
+
+    for (int i = 0; i < indx; i++)
+    {
+        int tmp = 0;
+        for (int j = 0; j < BOARD_SIZE; j++)
+            tmp += poss[i].poss[j];
+        if (tmp < leastOption && tmp > 0)
+        {
+            leastOption = tmp;
+            iWithLeastOptions = i;
+        }
+    }
+
+    int countOfBoards = 0;
+    if (iWithLeastOptions > -1)
+        for (int i = 0; i < BOARD_SIZE; i++)
+        {
+            if (poss[iWithLeastOptions].poss[i] == 1)
+            {
+
+                possBoard[countOfBoards].status = leastOption;
+                possBoard[countOfBoards].board[poss[iWithLeastOptions].cell] = i + 1;
+                // printf("CELL: %d, NUM: %d\n", poss[iWithLeastOptions].cell - 1, i + 1);
+                countOfBoards++;
+            }
+        }
+
+    delete[] poss;
 }
 
 __host__ void solve(int indx, int *sudokuBoard, int *targetCell, appeared *app, appeared *calculated, const int *start_board, cudaError_t &cudaStatus)
@@ -484,12 +586,13 @@ __host__ int solveSudoku(const int *start_board)
 {
     cudaError_t cudaStatus;
     int *sudokuBoard = 0;
-    int tmpSudokuBoard[CELL_COUNT];
-    possibilitie *poss_d = 0, *poss_h = 0;
+    int tmpSudokuBoard[CELL_COUNT * NUM_OF_THREADS];
+    possibleBoard *poss_d = 0, *poss_h = 0;
 
-    poss_h = new possibilitie[CELL_COUNT];
-
-    std::stack<board> S;
+    poss_h = new possibleBoard[BOARD_SIZE * NUM_OF_THREADS];
+    auto cmp = [](possibleBoard left, possibleBoard right)
+    { return (left.status) > (right.status); };
+    std::priority_queue<possibleBoard, std::vector<possibleBoard>, decltype(cmp)> S(cmp);
 
     // Choose which GPU to run on, change this on a multi-GPU system.
     cudaStatus = cudaSetDevice(0);
@@ -499,14 +602,14 @@ __host__ int solveSudoku(const int *start_board)
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void **)&sudokuBoard, CELL_COUNT * sizeof(int));
+    cudaStatus = cudaMalloc((void **)&sudokuBoard, NUM_OF_THREADS * CELL_COUNT * sizeof(int));
     if (cudaStatus != cudaSuccess)
     {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void **)&poss_d, CELL_COUNT * sizeof(possibilitie));
+    cudaStatus = cudaMalloc((void **)&poss_d, NUM_OF_THREADS * BOARD_SIZE * sizeof(possibleBoard));
     if (cudaStatus != cudaSuccess)
     {
         fprintf(stderr, "cudaMalloc failed!");
@@ -522,11 +625,6 @@ __host__ int solveSudoku(const int *start_board)
 
     printBoard((int *)start_board);
     runSolver<<<1, 1>>>(sudokuBoard, poss_d);
-    // While first check is run, we copy start board to tmp storage
-    for (int i = 0; i < CELL_COUNT; i++)
-    {
-        tmpSudokuBoard[i] = start_board[i];
-    }
 
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess)
@@ -542,37 +640,113 @@ __host__ int solveSudoku(const int *start_board)
         goto Error;
     }
 
-    cudaStatus = cudaMemcpy(poss_h, poss_d, CELL_COUNT * sizeof(possibilitie), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(poss_h, poss_d, NUM_OF_THREADS * BOARD_SIZE * sizeof(possibleBoard), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess)
     {
         fprintf(stderr, "cudaMemcpy failed! Returned error code %d\n", cudaStatus);
         goto Error;
     }
 
-    for (int i = 0; i < CELL_COUNT; i++)
+    for (int i = 0; i < NUM_OF_THREADS; i++)
     {
-        if (poss_h[i].cell == -1)
-            break;
         for (int j = 0; j < BOARD_SIZE; j++)
         {
-            if (poss_h[i].poss[j] == 1)
+            if (poss_h[i * BOARD_SIZE + j].status >= 1)
             {
-                tmpSudokuBoard[poss_h[i].cell - 1] = j;
-                board tmp;
+#ifdef DEBUG_MODE
+                printf("Possible board [THREAD: %d][POSS: %d]:\n", i + 1, j + 1);
+                printBoard(poss_h[i * BOARD_SIZE + j].board);
+#endif
+                possibleBoard tmp;
                 for (int k = 0; k < CELL_COUNT; k++)
-                    tmp.board[k] = tmpSudokuBoard[k];
+                    tmp.board[k] = poss_h[i * BOARD_SIZE + j].board[k];
+                tmp.status = poss_h[i * BOARD_SIZE + j].status;
                 S.push(tmp);
-                tmpSudokuBoard[poss_h[i].cell - 1] = 0;
             }
         }
     }
-    while (!S.empty())
-    {
-        board tmp = S.top();
-        S.pop();
 
-        // Now we have a board and will be generating new boards based on tmp
-        //
+    while (!S.empty() && S.size() < 20000)
+    {
+        // Input new boards
+        int indx = 0;
+#ifdef DEBUG_MODE
+        printf("%ld\n", S.size());
+#endif
+        for (; indx < NUM_OF_THREADS && !S.empty(); indx++)
+        {
+            possibleBoard tmp = S.top();
+            S.pop();
+#ifdef DEBUG_MODE
+            printf("Considering board:\n");
+            printBoard(tmp.board);
+#endif
+            for (int j = 0; j < CELL_COUNT; j++)
+            {
+                tmpSudokuBoard[indx * CELL_COUNT + j] = tmp.board[j];
+            }
+        }
+
+        // Copy memory and run kernel
+        cudaStatus = cudaMemcpy(sudokuBoard, tmpSudokuBoard, (NUM_OF_THREADS)*CELL_COUNT * sizeof(int), cudaMemcpyHostToDevice);
+        if (cudaStatus != cudaSuccess)
+        {
+            fprintf(stderr, "cudaMemcpy failed!");
+            goto Error;
+        }
+#ifdef DEBUG_MODE
+        printf("Running %02d threads\n", indx);
+#endif
+        runSolver<<<1, indx>>>(sudokuBoard, poss_d);
+
+        // Fetch resoults
+        cudaStatus = cudaGetLastError();
+        if (cudaStatus != cudaSuccess)
+        {
+            fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+            goto Error;
+        }
+
+        cudaStatus = cudaDeviceSynchronize();
+        if (cudaStatus != cudaSuccess)
+        {
+            fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+            goto Error;
+        }
+
+        cudaStatus = cudaMemcpy(poss_h, poss_d, NUM_OF_THREADS * BOARD_SIZE * sizeof(possibleBoard), cudaMemcpyDeviceToHost);
+        if (cudaStatus != cudaSuccess)
+        {
+            fprintf(stderr, "cudaMemcpy failed! Returned error code %d\n", cudaStatus);
+            goto Error;
+        }
+
+        // Add new boards to S
+        for (int i = 0; i < indx; i++)
+        {
+            for (int j = 0; j < BOARD_SIZE; j++)
+            {
+                if (poss_h[i * BOARD_SIZE + j].status >= 1)
+                {
+#ifdef DEBUG_MODE
+                    printf("Possible board [THREAD: %d][POSS: %d]:\n", i + 1, j + 1);
+                    printBoard(poss_h[i * BOARD_SIZE + j].board);
+#endif
+                    if (isBoardValid(poss_h[i * BOARD_SIZE + j].board))
+                    {
+                        printf("SOLVED!\n");
+                        printBoard(poss_h[i * BOARD_SIZE + j].board);
+                        goto Error;
+                    }
+
+                    possibleBoard tmp;
+                    for (int k = 0; k < CELL_COUNT; k++)
+                        tmp.board[k] = poss_h[i * BOARD_SIZE + j].board[k];
+                    tmp.status = poss_h[i * BOARD_SIZE + j].status;
+                    S.push(tmp);
+                }
+            }
+        }
     }
 
 Error:
